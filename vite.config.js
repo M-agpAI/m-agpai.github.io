e0d27429727aaf19a7b88aefca579e6ce0ee547a
Dibyakanti Kumar <dibyakantikumar1999@gmail.com>
2026-06-28 09:20:34 +0100

    Checked build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
   plugins: [react()],
-
-  // Change this to your GitHub repo name.
-  // Example: if your URL is https://username.github.io/magpai-web/
-  // then keep "/magpai-web/"
-  base: "/magpai-web/" // IMPORTANT for GitHub Pages
+  base: "/magpai/",
 });
